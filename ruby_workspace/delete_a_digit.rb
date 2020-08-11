f7236dd051d880b82e568cb768ead33de3f3df18
Charles A. Hartman <charles@hartman.dev>
2020-08-10 21:24:52 -0700

    kata work proceeding

---
+++ ruby_workspace/delete_a_digit.rb
@@ -0,0 +1,21 @@
+# delete_a_digit.rb - Simple Fun #79: Delete a Digit
+
+# Given an integer n, find the maximal number you can obtain by deleting
+# exactly one digit of the given number.
+
+# Example
+# For n = 152, the output should be 52;
+# For n = 1001, the output should be 101.
+# 
+# Input/Output
+# [input] integer n
+# Constraints: 10 ≤ n ≤ 1000000.
+# [output] an integer
+
+def delete_digit(n)
+  # CODE
+end
+
+delete_digit(152) == 52
+delete_digit(1001) == 101
+delete_digit(10) == 1
