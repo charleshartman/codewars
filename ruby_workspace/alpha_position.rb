8573219479574b110816f92a7dabf6e393e370b8
Charles A. Hartman <charles@hartman.dev>
2020-08-13 09:12:45 -0700

    kata work proceeding

--- ruby_workspace/alpha_position.rb
+++ ruby_workspace/alpha_position.rb
@@ -1,7 +1,26 @@
 # alpha_position.rb
 
-LETTERS = ('a'..'z').each_with_object([]) { |let, arr| arr << let }
-NUMBERS = (1..26).each_with_object([]) { |num, arr| arr << num }
+def alphabet_position(text)
+  dict = {}
+  num = 0
+  ('a'..'z').each { |letter| dict[letter] = (num += 1) }
+  result = text.downcase.chars.keep_if { |char| char =~ /[a-z]/ }
+  result = result.map { |letter| letter = dict[letter] }.join(' ')
+end
 
-p LETTERS
-p NUMBERS
+p alphabet_position("The sunset sets at twelve o' clock.") \
+  == "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
+p alphabet_position("-.-'") == ""
+
+
+# input: string
+# output: string on numbers, space delimited
+# data structure: hash, keys are letters, values numbers, array (result)
+# algorithm:
+#   - create hash dictionary (dict) of letter => number pairs
+#   - convert string to lowercase (result)
+#   - convert string to array (result)
+#   - remove non-letter chars from array
+#   - iterate through array, convert using dictionary 
+#   - join result 
+#   - return result string
--- ruby_workspace/triple_double.rb
+++ ruby_workspace/triple_double.rb
@@ -5,16 +5,27 @@
 # triple of a number at any place in num1 and also a straight double of
 # the same number in num2. If this isn't the case, return 0.
 
-def triple_double(num1, num2)
-  arr = (0..9).inject([]) { |arr, num| arr << num }
-  arr.each do |n|
-    if num1.to_s.match?(/[n{3}]/) && num2.to_s.match?(/[n{2}]/)
-      puts "works"
+def triple_double2(num1, num2)
+  toggle = nil
+  ('0'..'9').each do |n|
+    if num1.to_s.match?(/#{n}{3}/) && num2.to_s.match?(/#{n}{2}/)
+      toggle = 1
+      break
     else
-      puts "nope"
+      toggle = 0
     end
   end
+  toggle
+end
 
+def triple_double(num1, num2)
+  ('0'..'9').each do |n|
+    if num1.to_s.match?(/#{n}{3}/) && num2.to_s.match?(/#{n}{2}/)
+      return 1
+      break
+    end
+  end
+  return 0
 end
 
 # Examples
@@ -24,3 +35,16 @@ p triple_double(1222345, 12345) == 0
 # num1 has straight triple 2s but num2 has only a single 2
 p triple_double(12345, 12345) == 0
 p triple_double(666789, 12345667) == 1
+
+# input: two integer values
+# output: integer 0 or 1
+# data structure: array (arr), string, integers
+# algorithm:
+#   - initialize an array (0-9)
+#   - initialize toggle to hold 0 or 1, initial value nil
+#   - convert num1 and num2 to strings
+#   - for each number in the array, check for 3 successive occurrences
+#       in str1 and 2 successive occurrences in str2
+#       - iterate through arr(0-9) with conditional and toggle
+#   - if this occurs return 1
+#   - else return 0
