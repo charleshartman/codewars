dacc3ac618eddce79ad7229c9eabbe285becbf1a
Charles A. Hartman <charles@hartman.dev>
2020-08-16 20:25:08 -0700

    kata work proceeding

--- ruby_workspace/string_incrementer.rb
+++ ruby_workspace/string_incrementer.rb
@@ -1,15 +1,18 @@
 # string_incrementer.rb
 
 def increment_string(input)
-  work = input.partition(/\d+/).delete_if { |el| el == '' }
-  if work.length <= 1
+  work = input.partition(/\d+/)
+  work << work.pop.partition(/\d+/)
+  work = work.flatten.delete_if { |el| el == '' }
+  p work
+  if work.last == nil || work.last.match?(/[^0-9]/)
     work << '1'
   elsif work[1][-2] == '0'
     work[1][-1] = (work[1][-1].to_i + 1).to_s
   else
     work[1] = ((work[1].to_i) + 1).to_s
   end
-  work.join
+  p work.join
 end
 
 p increment_string("foo") == "foo1"
@@ -18,6 +21,9 @@ p increment_string("foobar1") == "foobar2"
 p increment_string("foobar00") == "foobar01"
 p increment_string("foobar99") == "foobar100"
 p increment_string("") == "1"
+p increment_string("f00b4r") == "f00b4r1"
+p increment_string("foobar00999") == "foobar01000"
+
 
 # str.chars.last.to_i.to_s == str.chars.last
 
