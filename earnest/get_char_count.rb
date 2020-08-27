87a11c948ee2477902a5ae5e58b4789b05513255
Charles A. Hartman <charles@hartman.dev>
2020-08-27 09:11:09 -0700

    kata work proceeding

---
+++ earnest/get_char_count.rb
@@ -0,0 +1,18 @@
+# get_char_count.rb
+# 6 kyu
+
+def get_char_count(str)
+  # code
+end
+
+p get_char_count("Mississippi") == {4=>["i", "s"], 2=>["p"], 1=>["m"]}
+p get_char_count("Hello. Hello? HELLO!!") == {6=>["l"], 3=>["e", "h", "o"]}
+p get_char_count("aaa...bb...c!") == {3=>["a"], 2=>["b"], 1=>["c"]}
+p get_char_count("aaabbbccc") == {3=>["a", "b", "c"]}
+p get_char_count("abc123") == {1=>["1", "2", "3", "a", "b", "c"]}
+
+=begin
+
+
+
+=end
