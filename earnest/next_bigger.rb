ef2f942c53bb3c1d14f1ae2b54ca25fd9b7abadc
Charles A. Hartman <charles@hartman.dev>
2020-09-03 09:28:35 -0700

    kata work proceeding, RB109 interview prep in earnest

--- earnest/next_bigger.rb
+++ earnest/next_bigger.rb
@@ -18,7 +18,12 @@ If no bigger number can be composed using those digits, return -1:
 =end
 
 def next_bigger(int)
-  # code
+  greatest = int.digits.sort.reverse.join.to_i
+  return -1 if greatest == int
+  
+  (int + 1).upto(greatest) do |num|
+    return num  if num.digits.sort.reverse.join.to_i == greatest
+  end
 end
 
 p next_bigger(12) == 21
@@ -32,6 +37,22 @@ p next_bigger(531) == -1
 
 =begin
 
+rules:
+  - move the digits in the given number in such a way as to form the next
+    greatest integer possible with those digits
+  - if the given number is already at the max for digits then return -1
 
+input: integer
+output: integer
+data strutture, integer, array
+
+algorithm:
+  - initialize local variable (greatest) and assign to it the value of given
+    integer reverse sorted
+  - the (greatest) possible number possible can be returned by sorting the digits
+    in reverse, use this as a test/guard to begin, return -1 if given number is
+    equal to (greatest)
+  - iterate from given number to (greatest), if iteration (num) sorted is equal
+    to greatest (already sorted) then return num
 
 =end
