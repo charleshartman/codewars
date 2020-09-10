4acbe0f1676875ecf323ea977e1821f18d5d8e62
Charles A. Hartman <charles@hartman.dev>
2020-09-09 21:18:23 -0700

    kata work proceeding, RB109 interview prep in earnest

--- earnest/find_the_missing_letter.rb
+++ earnest/find_the_missing_letter.rb
@@ -1,4 +1,5 @@
 # find_the_missing_letter.rb
+# 6 kyu
 
 # Write a method that takes an array of consecutive (increasing) letters as input
 # and that returns the missing letter in the array.
@@ -8,13 +9,16 @@
 # The array will always contain letters in only one case.
 
 # Example:
-# ['a','b','c','d','f'] -> 'e' ['O','Q','R','S'] -> 'P'
-
 # ["a","b","c","d","f"] -> "e"
 # ["O","Q","R","S"] -> "P"
+
 # (Use the English alphabet with 26 letters!)
 
-
+def find_missing_letter(arr)
+  arr.each_index do |idx|
+    return (arr[idx].ord + 1).chr if arr[idx].ord != (arr[idx + 1].ord) - 1
+  end
+end
 
 p find_missing_letter(["a","b","c","d","f"]) == "e"
 p find_missing_letter(["O","Q","R","S"]) == "P"
@@ -24,6 +28,20 @@ p find_missing_letter(["b","d","e"]) == "c"
 
 =begin
 
+problem: 
+  - find the missing letter in array. letters are in alpha order, either
+    all caps or all lowercase
+  - exactly one char is missing
+  - at least two chars in given array
 
+input: array of strings
+output: string
+data structure: array, string
+
+algorithm:
+  - iterate over given array
+    - find ordinal of first element
+    - if next element ordinal is not equal to previous ordinal + 1
+      - return ordinal + 1
 
 =end
