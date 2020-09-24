ef296e2b6769161a3350dc8a41fca02a8045f82c
Charles A. Hartman <charles@hartman.dev>
2020-09-24 16:13:31 -0700

    kata work proceeding, RB109 interview prep in earnest

--- earnest/equal_sides.rb
+++ earnest/equal_sides.rb
@@ -19,31 +19,51 @@
 # index.
 
 def find_even_index(arr)
-  #your code here
+  counter = 0
+  while counter < arr.length
+    left, right = [], []
+    arr.each_index do |idx|
+      left << arr[idx] if idx < counter
+      right << arr[idx] if idx > counter
+    end
+    return counter if left.sum == right.sum
+    counter += 1
+  end
+  -1
 end
 
-find_even_index([1, 2, 3, 4, 3, 2, 1]) == 3
-find_even_index([1, 100, 50, -51, 1, 1]) == 1
-find_even_index([1, 2, 3, 4, 5, 6]) == -1
-find_even_index([20, 10, 30, 10, 10, 15, 35]) == 3
-find_even_index([20, 10, -80, 10, 10, 15, 35]) == 0
-find_even_index([10, -80, 10, 10, 15, 35, 20]) == 6
-find_even_index(Array(1..100)) == -1
-find_even_index([0, 0, 0, 0, 0]) == 0
-find_even_index([-1, -2, -3, -4, -3, -2, -1]) == 3
-find_even_index(Array(-100..-1)) == -1
+p find_even_index([1, 2, 3, 4, 3, 2, 1]) == 3
+p find_even_index([1, 100, 50, -51, 1, 1]) == 1
+p find_even_index([1, 2, 3, 4, 5, 6]) == -1
+p find_even_index([20, 10, 30, 10, 10, 15, 35]) == 3
+p find_even_index([20, 10, -80, 10, 10, 15, 35]) == 0
+p find_even_index([10, -80, 10, 10, 15, 35, 20]) == 6
+p find_even_index(Array(1..100)) == -1
+p find_even_index([0, 0, 0, 0, 0]) == 0
+p find_even_index([-1, -2, -3, -4, -3, -2, -1]) == 3
+p find_even_index(Array(-100..-1)) == -1
 
 =begin
 
 <-PEDAC->
 problem:
-  - find the index number of an array
+  - find the index number of an array where the elements to the left of that
+    element summed equal with elements to the right of that element summed
+  - if this is not true of any index return -1
 
-input:
-output:
-data structure: 
+input: array
+output: integer
+data structure: array, integer 
 
 algorithm:
-  - 
+  - initialize (counter) to 0
+  - loop while (counter) is less than length of array
+    - initialize to empty arrays for (left) and (right)
+    - iterate through each index
+      - if index is less than counter push to left
+      - if index is greater than counter push to right
+        - if sum of left is equal to sum of right return counter
+      - increment (counter) by one
+  - without explicit return above return -1
 
 =end
--- earnest/sherlock_on_pockets.rb
+++ earnest/sherlock_on_pockets.rb
@@ -17,10 +17,57 @@ pockets = {
   jane: [7]
 } 
 
+pockets2 = { julia: nil, meg: [] }
+
 # Write method which helps Sherlock to find suspects. If no suspect is found
 # or there are no pockets (pockets == nil), the method should return nil.
+
+def find_suspects(pockets, permitted)
+  suspects = []
+
+  pockets.each do |person, contents|
+    next if contents == [] || contents == nil
+    contents.each do |item|
+      if !permitted.include?(item)
+        suspects << person
+      end
+    end
+  end
+  return nil if suspects.empty?
+  suspects.uniq
+end
 
 p find_suspects(pockets, [1, 2]) == [:tom, :jane]
 p find_suspects(pockets, [1, 7, 5, 2]) == nil
 p find_suspects(pockets, []) == [:bob, :tom, :jane]
 p find_suspects(pockets, [7]) == [:bob, :tom]
+p find_suspects(pockets2, [1, 6]) == nil
+
+=begin
+
+<-PEDAC->
+problem:
+  - return an array of keys from the given hash where the values include 
+    anything other of the given permitted items
+  - return nil if no suspects found or no pockets
+
+logic : 
+  return key if pockets[:key] includes something other than items in given
+  permitted array
+
+input: hash
+output: array or nil
+data structure: hash, array
+
+algorithm:
+  - initialize (suspects) array to []
+  - iterate through pockets
+    - go to next if pockets (contents) is empty or nil
+    - iterate through value subarray (contents)
+      - if any values in contents are NOT included in given array
+        - push key to (suspects)
+  - return nil if (suspects) is empty
+    - this might be one guard clause or two... return or reassignment
+  - return (suspects)
+
+=end
