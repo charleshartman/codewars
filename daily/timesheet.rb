8d53dce86367a9a104113319301da60958deeee2
Charles A. Hartman <charles@hartman.dev>
2021-02-02 15:31:54 -0800

    daily exercise

---
+++ daily/timesheet.rb
@@ -0,0 +1,24 @@
+# timesheet.rb
+
+# Background:
+
+# At work I need to keep a timesheet, by noting which project I was working on
+# every 15 minutes. I have an timer that beeps every 15 minutes to prompt me to
+# note down what I was working on at that point, but sometimes when I'm away
+# from my desk or working continuously on one project, I don't note anything
+# down and these get recorded as null.
+
+# Task:
+
+# Help me populate my timesheet by replacing any null values in the array with
+# the correct project name which is given by surrounding matching values.
+
+p fill_gaps([1,nil,1]) == [1,1,1]   # Replace nill values surrounded by matching values
+p fill_gaps([1,nil,nil,nil,1]) == [1,1,1,1,1]  # There may be multiple nils
+p fill_gaps([1,nil,1,2,nil,2]) == [1,1,1,2,2,2]  # There may be multiple replacements required
+p fill_gaps([1,nil,2,nil,2,nil,1]) == [1,nil,2,2,2,nil,1]  # No nesting.
+p fill_gaps([1,nil,2]) == [1,nil,2] # No replacement if ends don't match
+p fill_gaps([nil,1,nil]) == [nil,1,nil] # No replacement if ends don't match off the ends of the array
+p fill_gaps(['codewars', nil, nil, 'codewars', 'real work', nil, nil, 'real work']) \
+         == ["codewars", "codewars", "codewars", "codewars", "real work", \
+             "real work", "real work", "real work"] # Works with strings too
